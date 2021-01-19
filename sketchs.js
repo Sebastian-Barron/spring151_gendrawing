d505bb8e01831d5ef92f407a04b7edba02ae302d
Sebastian-Barron <67523744+Sebastian-Barron@users.noreply.github.com>
2021-01-19 13:38:11 -0600

    whats up?

--- sketchs.js
+++ sketchs.js
@@ -3,9 +3,6 @@ function setup() {
 }
 
 function draw(){
-    background(255,10,10);
-    rectangle(20,30,55,55)
-    ellipse(x, 100, 50,55);
-    x = x + 1
-
+    background(25,0,10);
+    rectangle(50,30,55,55)
 }
